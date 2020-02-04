fd524018a6f0949142827baba2116cd400e40a0b
zhouyiteng <542490754@qq.com>
2020-02-04 15:40:59 +0000

    first code

---
+++ self-learning/AppMain.java
@@ -0,0 +1,16 @@
+public class AppMain
+{
+    public static void main(String args[])
+    {
+        Doll beibei = new Doll("beibei");
+        Doll jingjing = new Doll("jingjing");
+        Doll huanhuan = new Doll("huanhuan");
+        Doll yingying = new Doll("yingying");
+        Doll nini = new Doll("nini");
+        beibei.speak();
+        jingjing.speak();
+        huanhuan.speak();
+        yingying.speak();
+        nini.speak();
+    }
+}
---
+++ self-learning/Doll.java
@@ -0,0 +1,14 @@
+public class Doll{
+    /** name */
+    private String name;
+    /** method */
+    public Doll(String name)
+    {
+        this.name=name; // set name
+    }
+    /** speak */
+    public void speak()
+    {
+        System.out.println(name); //print name
+    }
+}
