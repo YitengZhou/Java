5b033ae43c24020f01dac2b1d54b10493a8e0f23
zhouyiteng <542490754@qq.com>
2020-02-08 12:16:14 +0000

    finish

--- 02 Triangle/template/Triangle.java
+++ 02 Triangle/template/Triangle.java
@@ -23,12 +23,40 @@ class Triangle
     // Returns a printable string that describes this triangle
     public String toString()
     {
+
         return "(" + firstLength + "," + secondLength + "," + thirdLength + ")";
     }
 
     // Works out what kind of triangle this is !
     static TriangleType identifyTriangleType(int first, int second, int third)
     {
+        long first_l,second_l,third_l;
+        first_l = first;
+        second_l = second;
+        third_l =third;
+        if (first_l <= 0 || second_l <= 0 || third_l <= 0){
+            return TriangleType.Illegal;
+        }
+        if (first_l + second_l < third_l || second_l + third_l < first_l || first_l + third_l < second_l){
+            return TriangleType.Impossible;
+        }
+        if (first_l + second_l == third_l || second_l + third_l == first_l || first_l + third_l == second_l){
+            return TriangleType.Flat;
+        }
+        if (first_l*first_l + second_l*second_l == third_l*third_l
+                || second_l*second_l + third_l*third_l == first_l*first_l
+                || first_l*first_l + third_l*third_l == second_l*second_l){
+            return TriangleType.Right;
+        }
+        if (first_l == second_l && second_l == third_l){
+            return TriangleType.Equilateral;
+        }
+        if (first_l == second_l || second_l == third_l ||first_l == third_l){
+            return TriangleType.Isosceles;
+        }
+        if (first_l != second_l && second_l != third_l && first_l != third_l){
+            return TriangleType.Scalene;
+        }
         return null;
     }
 }
